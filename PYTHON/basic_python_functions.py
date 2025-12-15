abe7f8c94bceb30099c8110acbd9a4fad8adc2ab
farzana <sahalafarsu@.com>
2025-12-15 06:29:18 +0530

    some python basic examples

---
+++ PYTHON/basic_python_functions.py
@@ -0,0 +1,37 @@
+# print numbers from n to 0 (-n)
+def neg(n):
+    for i in range(n, 1):
+        print(i, end=" ")
+
+
+# print numbers from n-1 to 0 (+n)
+def pos(n):
+    for i in range(n - 1, -1, -1):
+        print(i, end=" ")
+
+
+# squares of natural numbers (1², 2², 3², …) that are ≤ x
+def printIncreasingPower(x):
+    i = 1
+    while i * i <= x:
+        print(i * i, end=" ")
+        i += 1
+
+
+# print N's multiplication table
+def multiplicationTable(N):
+    for i in range(1, 11):
+        print(N * i, end=" ")
+
+
+# print the numbers from x to 0 in decreasing order in a single line.
+def printInDecreasing(x):
+    while (x >= 0):
+        print(x, end=" ")
+        x -= 1
+
+
+# given a string s, print its characters at even indices
+def stringJumper(s):
+    for i in range(0,len(s),2):
+        print(s[i], end="")
--- PYTHON/python.md
+++ PYTHON/python.md
@@ -1,1 +1,6 @@
-(a >= 0) **!=** (b >= 0) is True only when one is positive and the other is negative.
+(a >= 0) **!=** (b >= 0) is True only when one is positive and the other is negative. (**!= is XOR operation**)
+By default, print() adds a newline (\n). To print without a newline, use the **end** parameter.                                                  *print(i, end=" ")* - it gives a single space
+*print(i, end="")* - it doesn't gives a space. printing character and separating characters by nothing.
+#### Key Characteristics of range()
+**Memory Efficiency:** range() does not store all values in memory but generates them on demand, making it ideal for large sequences.
+**Immutability:** A range object cannot be changed after creation
